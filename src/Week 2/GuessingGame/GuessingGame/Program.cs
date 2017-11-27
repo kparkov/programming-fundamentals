60b4efca525c0505c5f6b734949fc79e2bc4e1a5
Alex <alexhartfelt@gmail.com>
2017-11-27 21:36:39 +0100

    Øvelse til uge 2

--- src/Week 2/GuessingGame/GuessingGame/Program.cs
+++ src/Week 2/GuessingGame/GuessingGame/Program.cs
@@ -8,13 +8,40 @@ namespace GuessingGame
         {
             // We can get a new random number like this:
             int secretNumber = Computer.ThinkOfNumber();
-            Console.WriteLine("Jeg tænker på det meget hemmelige tal: " + secretNumber);
+            Console.WriteLine("Jeg tænker på et meget hemmeligt tal");
 
             // We can get a user guess like this:
-            int userGuess = User.GetGuess();
-            Console.WriteLine("Brugeren gættede på: " + userGuess);
+            int userGuess = 0;
+            bool UserIsRight = userGuess == secretNumber;
+            int numberofGuesses = 0;
 
-            Console.WriteLine("Brug while og if/else if/else til at lave det til et gættespil.");
+            while (!UserIsRight) 
+            {
+                userGuess = User.GetGuess();
+                Console.WriteLine("Brugeren gættede på: " + userGuess);
+
+                if (userGuess < secretNumber) 
+                {
+                    Console.WriteLine("Det var sgu for lavt");
+                    UserIsRight = false;
+                }
+               
+                else if (userGuess > secretNumber)
+                {
+                    Console.WriteLine("Det var sgu for højt");
+                    UserIsRight = false;
+                }
+                else 
+                {
+                    UserIsRight = true;   
+                }
+                numberofGuesses++;
+            }
+            Console.WriteLine("Du gættede mit tal, der var: " + secretNumber);
+            Console.WriteLine("Flot. Og kun på: " + numberofGuesses + " gæt");
+
+
+
         }
     }
 }
--- src/Week 2/IfElse/IfElse/Program.cs
+++ src/Week 2/IfElse/IfElse/Program.cs
@@ -16,6 +16,28 @@ namespace IfElse
 
             // EXERCISE: CAN WE GO SEE THE MOVIE?
             // Write an if/else that checks whether the customer fulfills all the requirements for seeing the movie, and write the result in the Console.
+            bool isOldEnough = age >= movieMinimumAgeRequirement;
+            bool hasEnoughMoney = money >= moviePrice;
+            bool canSeeMovie = (isOldEnough && hasEnoughMoney);
+
+            //Print kriterierne:
+            Console.WriteLine("Din alder er: " + age);
+            Console.WriteLine("og du har: " + money + " penge");
+            Console.WriteLine("Man skal være: " + movieMinimumAgeRequirement + " år gammel, og filmen koster " + moviePrice);
+
+            //selve tjek:
+            if (canSeeMovie == true) 
+            {
+                Console.WriteLine("Du må gerne se filmen!");
+                Console.WriteLine("Din alder er: " + age + " så den er: " + isOldEnough);
+                Console.WriteLine("Din pengebeholdning er: " + money + " så den er: " + hasEnoughMoney);
+            }
+            else 
+            {
+                Console.WriteLine("Du kan ikke se filmen");
+                Console.WriteLine("Din alder er: " + age + " så den er: " + isOldEnough);
+                Console.WriteLine("Din pengebeholdning er: " + money + " så den er: " + hasEnoughMoney);
+            }
 
             // EXERCISE: SPECIFIC FEEDBACK
             // We want to provide more specific feedback to the customer. If the customer cannot go and see the movie, we want to
