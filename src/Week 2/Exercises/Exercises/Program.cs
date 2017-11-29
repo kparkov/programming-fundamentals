6e0700f6fbfa664615aac5ec82ea053a4b7d819c
Lasse1971 <lwp@wihlborgrail.dk>
2017-11-29 23:13:48 +0100

    Week 2 solutions

--- src/Week 2/Exercises/Exercises/Program.cs
+++ src/Week 2/Exercises/Exercises/Program.cs
@@ -15,12 +15,30 @@ namespace Exercises
             int startNumber = 4;
             int stepNumber = 3;
 
-            // 1A. Write a while loop. It should run as many times as the "repeat" variable states. Write the same line of text to the console each time.
-            // 1B. Modify the outputted line so it writes the number of times the loop has been running on screen (1, 2, 3, 4, and so on).
-            // 1C. Modify the numbering, so it begins counting from "startNumber", and still adds 1 each time (4, 5, 6, 7, and so on). It should still run "repeat" TIMES in total. 
+            // 1A. Write a while loop. It should run as many times as the "repeat" variable states. Write the same line of text to the console each time. OK
+            // 1B. Modify the outputted line so it writes the number of times the loop has been running on screen (1, 2, 3, 4, and so on). OK
+            // 1C. Modify the numbering, so it begins counting from "startNumber", and still adds 1 each time (4, 5, 6, 7, and so on). It should still run "repeat" TIMES in total. OK
             // 1D. Modify the numbering, so it adds "stepNumber" instead of 1 each time (4, 7, 10, 13, and so on).
             // 1E. Your solution should use the variables above, and if their values are changed, it should be reflected when we run the loop. It should have no fixed upper limits for repeat.
             // 1F. In addition to writing the current number, also write whether that number is even or odd (4 is even, 7 is odd, 10 is even, 13 is odd, and so on).
+
+            int counter = startNumber;
+
+            while (counter < repeat * startNumber)
+            {
+                Console.WriteLine("Vi tæller:  " + counter);
+
+                if (counter % 2 ==0)
+                {
+                    Console.WriteLine("Det var et lige tal!");
+                }
+                else
+                {
+                    Console.WriteLine("Det var et ulige tal!");
+                }
+
+                counter = counter + stepNumber;
+            }
 
             // EXERCISE 2
 
