b245d83d1348c2a58b8cdfa89fbec96d05618ad3
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-12-14 13:28:25 +0100

    Solution MerryChristmas

--- src/Exercises/MerryChristmas/MerryChristmas/Program.cs
+++ src/Exercises/MerryChristmas/MerryChristmas/Program.cs
@@ -11,6 +11,45 @@ namespace MerryChristmas
             string greeting = "Merry christmas";
             string exclamation = "!";
             int hoCount = 3;
+
+            int counter = 0;
+
+            while (counter < hoCount)
+            {
+                Console.Write(laugh + " ");
+                counter += 1;
+            }
+
+            Console.WriteLine(greeting);
+
+            counter = 0;
+
+            while (counter < hoCount)
+            {
+                Console.Write(laugh.ToUpper() + " ");
+                counter += 1;
+            }
+
+            Console.WriteLine(greeting.ToUpper() + exclamation);
+
+            counter = 0;
+
+            while (counter < hoCount)
+            {
+                if (counter != 0)
+                {
+                    Console.Write(hyphen);
+                }
+
+                Console.Write(laugh.ToUpper());
+
+                counter += 1;
+            }
+
+            Console.WriteLine((" " + greeting + exclamation).ToUpper());
+
+
+            Console.ReadKey();
         }
     }
 }
