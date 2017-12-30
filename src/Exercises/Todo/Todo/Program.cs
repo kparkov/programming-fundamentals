4e5e7a9d095dcd8beab07b38300b2d2e22ea8e13
Muhraab <33451014+Muhraab@users.noreply.github.com>
2017-12-30 10:01:27 +0100

    Todo version 1

--- src/Exercises/Todo/Todo/Program.cs
+++ src/Exercises/Todo/Todo/Program.cs
@@ -7,6 +7,28 @@ namespace Todo
     {
         static void Main(string[] args)
         {
+            List<string> userTodo = new List<string>()
+            {
+
+            };
+
+            Console.WriteLine("Tillykke! Du har ingen opgaver");
+            Console.WriteLine("Tilføj ny opgave:");
+            string userAssignment = Console.ReadLine();
+
+            userTodo.Add(userAssignment);
+        
+
+            Console.WriteLine("Du har " + userTodo.Count + " opgaver på din liste.");
+
+            Console.WriteLine("- " + userTodo[0]);
+
+
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
