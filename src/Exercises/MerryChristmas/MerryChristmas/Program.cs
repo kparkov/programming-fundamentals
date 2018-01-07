f1fa337f8d28b23764baad21c2da928f8523e939
Alex <alexhartfelt@gmail.com>
2018-01-07 16:06:25 +0100

    Merry Christmas

--- src/Exercises/MerryChristmas/MerryChristmas/Program.cs
+++ src/Exercises/MerryChristmas/MerryChristmas/Program.cs
@@ -11,6 +11,44 @@ namespace MerryChristmas
             string greeting = "Merry christmas";
             string exclamation = "!";
             int hoCount = 3;
+
+
+            //### Opgave 1:
+            while (hoCount > 0)
+            {
+                Console.Write(laugh + " ");
+                hoCount--;
+            }
+            Console.WriteLine(greeting);
+            hoCount = 3;
+
+
+            //### Opgave 2
+            while (hoCount > 0)
+            {
+                Console.Write(laugh.ToUpper() + " ");
+                hoCount--;
+            }
+            Console.WriteLine(greeting.ToUpper() + exclamation);
+            hoCount = 3;
+
+
+            //### Opgave 3
+            while (hoCount > 0)
+            {
+                Console.Write(laugh.ToUpper());
+                hoCount--;
+                if (hoCount >= 1)
+                {
+                    Console.Write(hyphen);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine(greeting.ToUpper() + exclamation);
+            hoCount = 3;
         }
     }
 }
