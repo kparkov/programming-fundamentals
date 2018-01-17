ed08cc0ba4b15bc2594d04365b2fb2aab07cb690
Lasse Parkov <lwp@wihlborgrail.dk>
2018-01-17 12:08:11 +0100

    SU updated

--- src/Week 6/SU/SU/SUCalculatorApp.cs
+++ src/Week 6/SU/SU/SUCalculatorApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,8 +8,20 @@ namespace SU
     public class SuCalculatorApp
     {
         public void Run()
+
         {
-            Console.WriteLine("Indtast din alder: ");
+            //Get user educationintention
+            List<string> educationTypes = new List<string>()
+            {
+                "Ungdomsuddannelse",
+                "Videregående uddannelse",
+                "Anden uddannelse"
+            };
+
+
+            Console.WriteLine("Velkommen til SU-beregneren.");
+            BlankLine();
+            Console.WriteLine("Du skal indtaste nogle oplysninger om dig selv.");
             Console.ReadLine();
 
 
@@ -20,89 +33,99 @@ namespace SU
             decimal extrasToSuBase = CalculateExtraSU(income, residence);
             decimal suBase = 0;
             int income = 0;
+            string genericUserQuestion = ("Vælg fra menuen nedenfor:");
             bool continueProgram = true;
 
-            while (continueProgram) ;
+            while (continueProgram);
         }
-            //ungdomsudd = 1
-            //videregående = 2
-            //anden udd = 3
-            //hjemmeboende = 1
-            //udeboende = 2
-            //alder u. 18 = 1
-            // alder 18-19 = 2
-            //alder 20+ = 3
-            //edustart før 1/7-2014 = true
-            //edustart efter 1/7-2014 = false
-            
+                //ungdomsudd = 1
+                //videregående = 2
+                //anden udd = 3
+                //hjemmeboende = 1
+                //udeboende = 2
+                //alder u. 18 = 1
+                // alder 18-19 = 2
+                //alder 20+ = 3
+                //edustart før 1/7-2014 = true
+                //edustart efter 1/7-2014 = false
 
-        public decimal CalculateSuBase (int education, int residence, int age, bool eduStart, bool dispensation, bool continueProgram)
+
+        public decimal CalculateSuBase(int education, int residence, int age, bool eduStart, bool dispensation, bool continueProgram)
         {
-            if (dispensation) return 3907;
+                    if (dispensation) return 3907;
 
-            if (age < 18) continueProgram = false;
+                    if (age < 18) continueProgram = false;
 
-            if (education == 2 && residence == 2) return 6090;
+                    if (education == 2 && residence == 2) return 6090;
 
-            if (education == 2 && residence == 1 && eduStart) return 3028;
+                    if (education == 2 && residence == 1 && eduStart) return 3028;
 
-            if (education == 2 && residence == 1 && !eduStart) return 946;
+                    if (education == 2 && residence == 1 && !eduStart) return 946;
 
-            if (education == 1 && residence == 1 && eduStart)
-            {
-                if (age < 20) return 1348; else return 3028;
-            }
-            if (education == 1 && residence == 1 && !eduStart) return 946;
- 
+                    if (education == 1 && residence == 1 && eduStart)
+                    {
+                        if (age < 20) return 1348; else return 3028;
+                    }
+                    if (education == 1 && residence == 1 && !eduStart) return 946;
+
+                    if (education == 1 && residence == 2 && age < 20 && !dispensation)
+                    {
+                        Console.WriteLine("Det er usikkert om du kan få SU. Kontakt din studievejleder.");
+                        continueProgram = false;
+                    }
+
+                    return 0;
+
         }
-            
 
 
 
 
-        //Tillægsydelse
-        // hjemmeboende = 1
-        // udeboende = 2
 
-        public decimal CalculateExtraSU (int income, int residence)
+                //Tillægsydelse
+                // hjemmeboende = 1
+                // udeboende = 2
+
+        public decimal CalculateExtraSU(int income, int residence)
         {
-        if (residence == 1 && income < 100000) return 3000;
+                    if (residence == 1 && income < 100000) return 3000;
 
-        if (income >= 100000 && income < 299999) return 2000;
+                    if (income >= 100000 && income < 299999) return 2000;
 
-        if (income < 500000) return 1000;
+                    if (income < 500000) return 1000;
 
-        else return 0;
+                    else return 0;
         }
-        
 
-        public static void Presentation (decimal sUbase, decimal extrasToSuBase)
+
+        public static void Presentation(decimal sUbase, decimal extrasToSuBase)
         {
-            Console.WriteLine ("Du er berettiget til "+ sUbase + " kr. i grundydelse");
-            BlankLine();
-            if (extrasToSuBase == 0)
-            {
-                Console.WriteLine("Du er desværre ikke berettiget til tillæg");
-            }
-            else
-            {
-                Console.WriteLine("Derudover er du berettiget til " + extrasToSuBase + " kr. i tillægsydelse.");
-            }
-            
-            
+                    Console.WriteLine("Du er berettiget til " + sUbase + " kr. i grundydelse");
+                    BlankLine();
+                    if (extrasToSuBase == 0)
+                    {
+                        Console.WriteLine("Du er desværre ikke berettiget til tillæg");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Derudover er du berettiget til " + extrasToSuBase + " kr. i tillægsydelse.");
+                    }
+
+
         }
 
         public static void BlankLine()
         {
-            Console.WriteLine("");
+                    Console.WriteLine("");
         }
-        
 
 
 
-        //int.ToString("C"); => "C" for Currency, laver korrekt opstilling med separatorer.
+
+                //int.ToString("C"); => "C" for Currency, laver korrekt opstilling med separatorer.
 
     }
+}
     
     
 
