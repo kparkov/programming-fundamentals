1b3185755a602afb3e5060528388a3cfcd1df5fb
Lasse Parkov <lwp@wihlborgrail.dk>
2018-01-29 21:08:50 +0100

    Gladiator 29.01.2018

---
+++ src/Project/Lasse_Gladiator/Gladiator/Gladiator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gladiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstwarrior = new Gladiator("Ibbermand");
+            var secondwarrior = new Gladiator("Jørgen");
+            firstwarrior.PresentYourself();
+            secondwarrior.PresentYourself();
+
+            firstwarrior.Attack(secondwarrior);
+
+            secondwarrior.PresentYourself();
+
+
+            Console.ReadKey();
+        }
+    }
+}
