4abf464824c601a5d89f9ec3b7ccda813c6b7cf4
Helcamped <33688405+Helcamped@users.noreply.github.com>
2018-01-29 21:10:39 +0100

    GladiatorPart1

---
+++ src/Project/Helena_gladiator/Gladiator/Gladiator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gladiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstwarrior = new Gladiator("Spartacus");
+            var secondwarrior = new Gladiator("Maximus");
+
+            firstwarrior.PresentYourself();
+            secondwarrior.PresentYourself();
+
+            while (secondwarrior.IsAlive())
+            {
+                firstwarrior.Attack(secondwarrior);
+                secondwarrior.PresentYourself();
+            }
+
+            firstwarrior.Attack(secondwarrior);
+
+            firstwarrior.PresentYourself();
+            secondwarrior.PresentYourself();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
